8cc1d39c9f9e03675e585ca95484d722401dc4dc
sumukha <ar-sumukha.m@rakuten.com>
2020-08-03 11:55:57 +0530

    feat(graph): bar graph for state wise is loaded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,27 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderNavComponent } from './components/header-nav/header-nav.component' 
+import { HeaderNavComponent } from './components/header-nav/header-nav.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatListModule} from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 import { FlexLayoutModule } from "@angular/flex-layout";
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
 import { HttpClientModule } from '@angular/common/http';
 import { TableViewComponent } from './components/table-view/table-view.component';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { HomepageComponent } from './components/homepage/homepage.component';
 import { GraphsComponent } from './components/graphs/graphs.component';
 import { GoogleChartsModule } from 'angular-google-charts';
-
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 @NgModule({
   declarations: [
     AppComponent,
@@ -50,7 +51,10 @@ import { GoogleChartsModule } from 'angular-google-charts';
     HttpClientModule,
     MatFormFieldModule,
     MatInputModule,
-    GoogleChartsModule
+    GoogleChartsModule,
+    MatAutocompleteModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/graphs/graphs.component.ts
+++ src/app/components/graphs/graphs.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CovidApiService } from '../../services/covid-api.service'
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
 @Component({
   selector: 'app-graphs',
   templateUrl: './graphs.component.html',
@@ -10,34 +13,50 @@ export class GraphsComponent implements OnInit {
   constructor(private _covidAPI: CovidApiService) { }
 
   title = 'Number of COVID-19 confirmed cases';
-   type = 'LineChart';
-   recoveredData=[]
-   confirmData=[]
-   deceasedData=[]
-   data = [
-   ];
-   //columnNames = ['Browser', 'Percentage'];
-   options = {    
-    colors: ['#e0440e', '#e6693e', '#ec8f6e', '#f3b49f', '#f6c7b6'], 
+  type = 'LineChart';
+  recoveredData = []
+  confirmData = []
+  deceasedData = []
+  data = [
+  ];
+  //columnNames = ['Browser', 'Percentage'];
+  options = {
+    colors: ['#e0440e', '#e6693e', '#ec8f6e', '#f3b49f', '#f6c7b6'],
     is3D: true,
     hAxis: {
       title: 'Date'
-   },
-   vAxis:{
-      title: 'Number of COVID-19 confirmed cases'
-   },
- pointSize:5,
- crosshair:{
-  color:'#000000',
-  trigger:'selection'  
- }
-   };
-   width = 550;
-   height = 400;
+    },
+    // vAxis: {
+    //   title: 'Number of COVID-19 confirmed cases'
+    // },
+    pointSize: 5,
+    crosshair: {
+      color: '#000000',
+      trigger: 'selection'
+    }
+  };
+  width = 550;
+  height = 400;
+
+  stateTitle = "COVID-19 information in"
+  stateType = 'BarChart';
+  stateData = []
+  stateGraphOptions = {};
+
+  stateActiveCases = 0;
+  stateName = "";
+  stateConfirmed = 0
+  stateRecovered = 0
+  stateDeceased = 0
+  stateObject = {}
+
+  myControl = new FormControl();
+  stateoptions: string[] = [];
+  filteredOptions: Observable<string[]>;
 
   ngOnInit(): void {
     this._covidAPI.getCasesByDate().subscribe(
-      (res)=>{
+      (res) => {
         res["cases_time_series"].forEach(element => {
           console.log(element)
           this.confirmData.push([element["date"], +element["totaldeceased"]])
@@ -47,23 +66,77 @@ export class GraphsComponent implements OnInit {
         })
       }
     )
+
+    this._covidAPI.getStates().subscribe(
+      (res) => {
+        Object.keys(res).forEach(key => {
+          if (key !== "State Unassigned") {
+            this.stateName = key;
+            Object.keys(res[key]).forEach(district => {
+              if (district === "districtData") {
+                Object.keys(res[key][district]).forEach(city => {
+                  this.stateActiveCases = this.stateActiveCases + res[key][district][city]["active"];
+                  this.stateConfirmed = this.stateConfirmed + res[key][district][city]["confirmed"];
+                  this.stateRecovered = this.stateRecovered + res[key][district][city]["recovered"];
+                  this.stateDeceased = this.stateDeceased + res[key][district][city]["deceased"]
+                })
+              }
+            })
+            this.stateTitle = "COVID-19 information in Karnataka State"
+            this.stateObject[this.stateName] = { active: this.stateActiveCases, recovered: this.stateRecovered, deceased: this.stateDeceased, confirmed: this.stateConfirmed }
+          }
+        })
+        this.stateData = [
+          ["Active", this.stateObject["Karnataka"].active],
+          ["Confirmed", this.stateObject["Karnataka"].confirmed],
+          ["Recovered", this.stateObject["Karnataka"].recovered],
+          ["Deceased", this.stateObject["Karnataka"].deceased]
+        ]
+        console.log(this.stateObject)
+        this.stateoptions = Object.keys(this.stateObject)
+      }
+    )
+
+    this.filteredOptions = this.myControl.valueChanges
+      .pipe(
+        startWith(''),
+        map(value => this._filter(value))
+      );
   }
 
-  onClickConfimButton(event){
-    this.data=this.confirmData;
-    this.title ='Number of COVID-19 Confirmed cases'
-  }
-  onClickRecoveredButton(event){
-    this.data=this.recoveredData;
-    this.title ='Number of COVID-19 Recovered cases'
-  }
-  onClickDeceasedButton(event){
-    this.data=this.deceasedData;
-    this.title ='Number of COVID-19 Deceased cases'
-  }
-  last15Days(event){
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
 
-    this.data=this.data.slice( this.data.length-15,this.data.length);
-    this.title= this.title+" in last 15 days"
+    return this.stateoptions.filter(option => option.toLowerCase().includes(filterValue));
+  }
+
+  onClickConfimButton(event) {
+    this.data = this.confirmData;
+    this.title = 'Number of COVID-19 Confirmed cases'
+  }
+  onClickRecoveredButton(event) {
+    this.data = this.recoveredData;
+    this.title = 'Number of COVID-19 Recovered cases'
+  }
+  onClickDeceasedButton(event) {
+    this.data = this.deceasedData;
+    this.title = 'Number of COVID-19 Deceased cases'
+  }
+  last15Days(event) {
+
+    this.data = this.data.slice(this.data.length - 15, this.data.length);
+    this.title = this.title + " in last 15 days"
+  }
+
+  onStateChange(event) {
+    console.log(event.source["_element"]["nativeElement"]["outerText"])
+
+    this.stateData = [
+      ["Active", this.stateObject[event.source["_element"]["nativeElement"]["outerText"]].active],
+      ["Confirmed", this.stateObject[event.source["_element"]["nativeElement"]["outerText"]].confirmed],
+      ["Recovered", this.stateObject[event.source["_element"]["nativeElement"]["outerText"]].recovered],
+      ["Deceased", this.stateObject[event.source["_element"]["nativeElement"]["outerText"]].deceased]
+    ]
+    this.stateTitle = "COVID-19 information in " + event.source["_element"]["nativeElement"]["outerText"] + " State"
   }
 }
