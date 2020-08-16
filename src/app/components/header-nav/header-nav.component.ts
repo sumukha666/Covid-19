0e127e7bf7415f6ecee402705d3936160ba61a38
Sumukha <sumukhamohan6@gmail.com>
2020-08-16 11:05:45 +0530

    feat(nav-bar): added back button in header

--- src/app/components/header-nav/header-nav.component.ts
+++ src/app/components/header-nav/header-nav.component.ts
@@ -1,26 +1,34 @@
-import { Component, Input, OnInit, Output, EventEmitter} from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
+import { Component, Input, OnInit, Output, EventEmitter } from "@angular/core";
+import { BreakpointObserver, Breakpoints } from "@angular/cdk/layout";
+import { Observable } from "rxjs";
+import { map, shareReplay } from "rxjs/operators";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-header-nav',
-  templateUrl: './header-nav.component.html',
-  styleUrls: ['./header-nav.component.scss']
+  selector: "app-header-nav",
+  templateUrl: "./header-nav.component.html",
+  styleUrls: ["./header-nav.component.scss"],
 })
-export class HeaderNavComponent implements OnInit{
+export class HeaderNavComponent implements OnInit {
   @Input() columnsToDisplay;
   @Input() stateNameToDisplay;
   @Input() showStateGraph;
-  ngOnInit(){
-    
-  }
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+  @Input() displayBackButton;
+  @Input() backToUrl;
+  ngOnInit() {}
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
     .pipe(
-      map(result => result.matches),
+      map((result) => result.matches),
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private route: Router
+  ) {}
 
+  onClickBackButton() {
+    this.route.navigate([this.backToUrl]);
+  }
 }
